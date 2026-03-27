bbf35005489752f877c610e58848a757045b402d
Maximilian Mace <maximilianmace@gmail.com>
2026-03-27 13:50:55 -0700

    bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/DnDTracker/', // <--- this fixes the blank page issue
   plugins: [react()],
 })
